875de81ccc2abc665412e497ef1dc6b6decc4cef
Halo O. Wang <1974533445@qq.com>
2022-10-16 20:48:15 +0800

    优化页面效果

--- DebugServer/DebugDict/views.py
+++ DebugServer/DebugDict/views.py
@@ -13,8 +13,12 @@ class SearchView(generic.View):
     form_class = SubmitForm
     template_name = 'index.html'
 
+    @staticmethod
+    def format_html_str(s):
+        return s.replace('\n', ' ').replace('\\', '\\\\')
+
     def get(self, request):
-        return render(request, self.template_name, {'has_cluster': False, 'has_article': False, 'err_msg_placeholder': "", 'scene_placeholder': ""})
+        return render(request, self.template_name, {'has_cluster': False, 'has_article': False, "article_num": 0, 'err_msg_placeholder': "", 'scene_placeholder': ""})
 
     def post(self, request):
         form_data2 = self.form_class(request.POST)
@@ -33,33 +37,39 @@ class SearchView(generic.View):
                 render_data['has_cluster'] = True
                 render_data['cluster'] = []
                 for t in range(len(ranked_common_sstr_origin)):
-                    common_sstr_dic = {'text': ranked_common_sstr_origin[t], 'num': len(ranked_aid_list[t]), 'article': []}
+                    common_sstr_dic = {'idx': len(render_data['cluster']), 'text': ranked_common_sstr_origin[t], 'num': len(ranked_aid_list[t]), 'article': []}
                     for aid in ranked_aid_list[t]:
-                        common_sstr_dic['article'].append({'title': SearchEng.base_data[aid].title, 'link': SearchEng.base_data[aid].link})
+                        if len(SearchEng.base_data[aid].content) > 100:
+                            text = SearchEng.base_data[aid].content[:100] + "..."
+                        else:
+                            text = SearchEng.base_data[aid].content
+                        text = text.replace('\n', ' ')
+                        common_sstr_dic['article'].append({'title': SearchEng.base_data[aid].title, 'text': text, 'link': SearchEng.base_data[aid].link})
                     render_data['cluster'].append(common_sstr_dic)
             else:
                 render_data['has_cluster'] = False
             # 构造全部解决方案的链接
             if aid_list:
                 render_data['has_article'] = True
+                render_data['article_num'] = len(aid_list)
                 render_data['all_articles'] = []
-                if len(aid_list) > 100:
-                    article_num = 100
-                    render_data['article_num_msg'] = "前100"
-                else:
-                    article_num = len(aid_list)
-                    render_data['article_num_msg'] = "全部" + str(article_num)
-                for t in range(article_num):
+                for t in range(len(aid_list)):
                     aid = aid_list[t]
-                    solve_dic = {'title': SearchEng.base_data[aid].title, 'link': SearchEng.base_data[aid].link}
-                    solve_msg_1article = solve_msgs[t].replace('\n', ' ')
-                    if len(solve_msg_1article) >= 100:
-                        solve_dic['solve_msg'] = solve_msg_1article[:100]
+                    solve_dic = {'title': self.format_html_str(SearchEng.base_data[aid].title), 'link': self.format_html_str(SearchEng.base_data[aid].link)}
+                    solve_msg_1article = self.format_html_str(solve_msgs[t])
+                    if len(solve_msg_1article) > 100:
+                        solve_dic['solve_msg'] = solve_msg_1article[:100] + "..."
                     else:
                         solve_dic['solve_msg'] = solve_msg_1article
+                    text = self.format_html_str(SearchEng.base_data[aid].content)
+                    if len(text) > 100:
+                        solve_dic['text'] = text[:100] + "..."
+                    else:
+                        solve_dic['text'] = text
                     render_data['all_articles'].append(solve_dic)
             else:
                 render_data['has_article'] = False
+                render_data['article_num'] = 0
             return render(request, self.template_name, render_data)
         else:
             pass
